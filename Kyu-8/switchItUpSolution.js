d12deef0b14f879926a05bc8eff953efddb3a3b9
Thi Thach <juliet.thi.thach@gmail.com>
2024-05-08 08:55:05 -0400

    complete switchItUp kata

---
+++ Kyu-8/switchItUpSolution.js
@@ -0,0 +1,15 @@
+function switchItUp(number) {
+  const nums = [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ];
+  return number <= 9 > 0 ? nums[number] : number;
+}
